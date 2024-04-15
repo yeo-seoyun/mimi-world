86848dec487f802eb436e93b720018bdde5ecb71
yeoseoyun <yeosummer94@gmail.com>
2024-04-15 23:44:14 +0900

    [Refactor] :    컴포넌트 분리

---
+++ src/pages/Home/AgeLine.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+function AgeLine() {
+  return (
+    <div className="font-mimi bg-pink-100 w-full h-full flex flex-col gap-8">
+      <h2 className="text-2xl m-auto">Age-Line</h2>
+      <div className="flex">
+        <Link to="">3~</Link>
+        <Link to="">3~</Link>
+        <Link to="">3~</Link>
+        <Link to="">3~</Link>
+        <Link to="">3~</Link>
+        <Link to="">3~</Link>
+        <Link to="">3~</Link>
+      </div>
+    </div>
+  );
+}
+
+export default AgeLine;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,18 @@
+import AgeLine from "./AgeLine";
 import ImageSlider from "./ImageSlider";
 import NewProductSwiper from "./NewProductSwiper";
+import Youtube from "./Youtube";
 
 function Home() {
   return (
-    <section className="py-16">
+    <section className="py-16 flex flex-col gap-14">
       <article>
         <ImageSlider />
       </article>
-      <article className="w-[1440px] h-[50vh] m-auto">
+      <article className="w-[1440px] m-auto flex flex-col gap-14">
         <NewProductSwiper />
+        <AgeLine />
+        <Youtube />
       </article>
     </section>
   );

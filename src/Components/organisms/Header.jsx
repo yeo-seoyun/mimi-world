08065b9feda76b47a92f88df684440c8b6d6faf6
yeoseoyun <yeosummer94@gmail.com>
2024-04-15 18:37:34 +0900

    [Design] :   헤더 드롭다운

--- src/Components/organisms/Header.jsx
+++ src/Components/organisms/Header.jsx
@@ -3,20 +3,24 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <>
-      <header className="w-full px-12 top-0 z-50 bg-white bg-opacity-70 shadow-default flex justify-between items-center ">
-        <div className="w-full flex gap-8">
-          <div className="group pr-16 inline-block relative">
-            <Link to="">미미월드</Link>
-            <ul className="absolute w-full py-4 px-2 hidden group-hover:block z-50 bg-white text-sm space-y-3">
-              <li>회사소개</li>
+      <header className="container w-full px-12 top-0 z-50 bg-white bg-opacity-70 shadow-default flex justify-between items-center ">
+        <div className="w-full flex">
+          <div className="group inline-block w-[6.875rem]">
+            <Link to="" className="relative">
+              미미월드
+            </Link>
+            <ul className="absolute left-0 w-full py-4 px-12 hidden group-hover:block z-50 bg-white text-sm space-y-3">
+              <li className="bg-pink-200">회사소개</li>
               <li>CEO인사말</li>
               <li>인재채용</li>
             </ul>
           </div>
 
-          <div className="group pr-16 inline-block relative">
-            <Link to="">브랜드</Link>
-            <ul className="absolute w-full py-4 px-2 hidden group-hover:block z-50 bg-white text-sm space-y-3">
+          <div className="group inline-block w-[6.875rem]">
+            <Link to="" className="relative">
+              브랜드
+            </Link>
+            <ul className="absolute left-0 w-full py-4 px-12 hidden group-hover:block z-50 bg-white text-sm space-y-3">
               <li>프린세스 미미</li>
               <li>미미프렌즈</li>
               <li>미미펫샵</li>
@@ -26,18 +30,18 @@ function Header() {
             </ul>
           </div>
 
-          <div className="group pr-16 inline-block relative">
+          <div className="group inline-block w-[6.875rem]">
             <Link to="">상품</Link>
-            <ul className="absolute w-full py-4 px-2 hidden group-hover:block z-50 bg-white text-sm space-y-3">
+            <ul className="absolute left-0 w-full py-4 px-12 hidden group-hover:block z-50 bg-white text-sm space-y-3">
               <li>신상품</li>
               <li>브랜드별</li>
               <li>연령별</li>
             </ul>
           </div>
 
-          <div className="group pr-16 inline-block relative">
+          <div className="group inline-block w-[6.875rem]">
             <Link to="">공지사항</Link>
-            <ul className="absolute w-full py-4 px-2 hidden group-hover:block z-50 bg-white text-sm space-y-3">
+            <ul className="absolute left-0 w-full py-4 px-12 hidden group-hover:block z-50 bg-white text-sm space-y-3">
               <li>FAQ</li>
               <li>공지사항</li>
               <li>이벤트</li>

38269a6099a1febdc4853ddfc2fbfaf5f52e0185
yeoseoyun <yeosummer94@gmail.com>
2024-04-16 12:21:05 +0900

    [Design] :   youtube 추가

--- src/pages/Home/Youtube.jsx
+++ src/pages/Home/Youtube.jsx
@@ -1,10 +1,23 @@
 import YouTube from "react-youtube";
 
 function Youtube() {
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const onReady = (event) => {
+    event.target.pauseVideo();
+  };
+
   return (
     <div className="font-mimi flex flex-col gap-6 items-center">
-      <h2 className="text-2xl">🐰미미 스마트워치🐰</h2>
-      <YouTube />
+      <h2 className="text-2xl">🐰미미 스마트워치🐰</h2>{" "}
+      <YouTube videoId="zsyhLF42LRg" opts={opts} onReady={onReady} />
     </div>
   );
 }

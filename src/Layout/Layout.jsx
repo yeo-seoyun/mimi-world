8b535f3c644d7d9ddc00bec2db2eccc3ae0f689d
yeoseoyun <yeosummer94@gmail.com>
2024-04-29 21:48:07 +0900

    [Build] :

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,9 +1,9 @@
 import { Outlet } from "react-router-dom";
 import { Provider } from "jotai";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import ScrollToTop from "../components/ScrollToTop";
-import Header from "../components/organisms/Header";
-import Footer from "../components/organisms/Footer";
+import ScrollToTop from "../components/ScrollToTop.jsx";
+import Header from "../components/organisms/Header.jsx";
+import Footer from "../components/organisms/Footer.jsx";
 
 function Layout() {
   const queryClient = new QueryClient();
